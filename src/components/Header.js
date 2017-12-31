95553fa07d19dcc29920fd1788d54db7adb7f636
Unknown <waljesusme2013@gmail.com>
2017-12-30 16:54:40 -0800

    Reactrouting is done

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+ 
+const Header = ({ title }) => (
+    <div>
+        <h1>{title}</h1>
+        <a href="/">Home</a>
+        <a href="/search">Search</a>
+        <a href="/mine">Mine</a>
+        <a href="/about">About</a>
+    </div>
+);
+
+export default Header;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,31 @@
-alert("it's alive");
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { connect } from 'react-redux';
+import { provider } from 'react-redux';
+import { Home, Mine,  About, Search } from './screens';
+
+const Index = ({pathname}) => {
+  switch(pathname) {
+    case '/search':
+     return <Search />;
+     case '/about':
+      return <About />;
+        case '/mine':
+        return <Mine />;
+      default: 
+      return <Home />;
+
+     }
+    }    
+
+let pathname = window.location.pathname;
+
+
+ReactDOM.render(<Index pathname={pathname} />, 
+  document.getElementById('root'));
+
+window.addEventListener('popstate', () => {
+    pathname = window.location.pathname;
+});
+
